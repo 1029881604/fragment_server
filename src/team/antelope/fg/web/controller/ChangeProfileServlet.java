cb6180a06cfc2e55f088bb0e1f4094ef8340ada2
Carlos <349811948@qq.com>
2018-04-07 19:51:37 +0800

    yy 修改servlet 不用配置web.xml

--- src/team/antelope/fg/web/controller/ChangeProfileServlet.java
+++ src/team/antelope/fg/web/controller/ChangeProfileServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import team.antelope.fg.service.impl.ChangeProfileServiceImpl;
-
+@WebServlet(name="ChangeProfileServlet", urlPatterns={"/ChangeProfileServlet"})  
 public class ChangeProfileServlet extends HttpServlet {
   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

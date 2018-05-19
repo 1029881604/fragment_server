becea39fc46a6f3c6954a3b3675c19f5b9af9428
uniquelry <1909506001@qq.com>
2018-05-20 00:03:40 +0800

    lry 提交了发布技能和需求的相关Servlet

--- src/team/antelope/fg/service/IPublishService.java
+++ src/team/antelope/fg/service/IPublishService.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import team.antelope.fg.entity.PersonNeed;
 import team.antelope.fg.entity.PersonSkill;
+import team.antelope.fg.entity.PublishNeed;
+import team.antelope.fg.entity.PublishSkill;
 
 /**
  * @author uniquelry
  * @Date 2018年4月9日 下午11:16:55
- * @Description 
+ * @Description 发布相关服务的接口
  */
 public interface IPublishService {
 	List<PersonSkill> getAllPersonSkill();
 	List<PersonNeed> getAllPersonNeed();
+	int publishNeed(PublishNeed need);
+	int publishSkill(PublishSkill skill);
 }
--- src/team/antelope/fg/service/impl/PublishServiceImpl.java
+++ src/team/antelope/fg/service/impl/PublishServiceImpl.java
@@ -8,12 +8,14 @@ import team.antelope.fg.dao.impl.PublishNeedDaoImpl;
 import team.antelope.fg.dao.impl.PublishSkillDaoImpl;
 import team.antelope.fg.entity.PersonNeed;
 import team.antelope.fg.entity.PersonSkill;
+import team.antelope.fg.entity.PublishNeed;
+import team.antelope.fg.entity.PublishSkill;
 import team.antelope.fg.service.IPublishService;
 
 /**
  * @author uniquelry
  * @Date 2018年4月9日 下午11:16:23
- * @Description 
+ * @Description 发布相关服务的实现类
  */
 public class PublishServiceImpl implements IPublishService {
 
@@ -28,6 +30,16 @@ public class PublishServiceImpl implements IPublishService {
 	@Override
 	public List<PersonNeed> getAllPersonNeed() {
 		return publishNeedDao.queryAllPersonNeed();
+	}
+
+	@Override
+	public int publishNeed(PublishNeed need) {
+		return publishNeedDao.insert(need);
+	}
+
+	@Override
+	public int publishSkill(PublishSkill skill) {
+		return publishSkillDao.insert(skill);
 	}
 
 }

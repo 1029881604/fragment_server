aca0409334f65e46452b153dd4baf66cf426fd85
Carlos <349811948@qq.com>
2018-05-20 11:21:51 +0800

    更新5_fragment_yy文件

--- sql/5_fragment_yy.sql
+++ sql/5_fragment_yy.sql
@@ -2,9 +2,10 @@
 -- ----------------------------
 -- Procedure structure for sp_all_attention
 -- ----------------------------
+----找除自己本身的person  start---
 DROP PROCEDURE IF EXISTS `sp_all_attention`;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_all_attention`(IN `_id` int)
+CREATE  PROCEDURE `sp_all_attention`(IN `_id` int)
 BEGIN
 
 select id, name, email, sex, age, headimg, starnum, dealnum,  fansnum
@@ -14,3 +15,24 @@ select id, name, email, sex, age, headimg, starnum, dealnum,  fansnum
 END
 ;;
 DELIMITER ;
+----找除自己本身的person  end---
+
+----找关注的人  start---
+drop procedure if exists sp_find_following;
+delimiter //  
+create procedure sp_find_following (
+	in _id int			
+)	 
+begin 
+select id, name, email, sex, age, headimg, starnum, dealnum,  fansnum
+	from v_person 
+	where exists (
+	select 1 from (
+		select attentionuserid fid from attention where uid= _id 
+		) friend
+	where friend.fid = v_person.id
+	);
+end;
+//
+delimiter ;
+----找关注的人  end---
